4bafd062adeaada9820dfacfb7c41ba80c9e348a
joneikholmkea <jone@kea.dk>
2023-09-04 09:12:01 +0200

    first

--- src/main/java/com/example/springstart/SpringstartApplication.java
+++ src/main/java/com/example/springstart/SpringstartApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringstartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringstartApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringstartApplication.class, args);
+    }
 
 }
